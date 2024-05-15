b6604d3412572cf85a72ebcafb8be3bb5c68c88f
jcopado <jcopado@surlabs.es>
2024-05-15 12:45:25 +0200

    Plugin base

---
+++ plugin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the LiveVoting Repository Object plugin for ILIAS.
+ * This plugin allows to create real time votings within ILIAS.
+ *
+ * The LiveVoting Repository Object plugin for ILIAS is open-source and licensed under GPL-3.0.
+ * For license details, visit https://www.gnu.org/licenses/gpl-3.0.en.html.
+ *
+ * To report bugs or participate in discussions, visit the Mantis system and filter by
+ * the category "LiveVoting" at https://mantis.ilias.de.
+ *
+ * More information and source code are available at:
+ * https://github.com/surlabs/LiveVoting
+ *
+ * If you need support, please contact the maintainer of this software at:
+ * info@surlabs.es
+ *
+ */
+
+$id = 'xlvo';
+
+$version = '1.9.0';
+
+$ilias_min_version = '8.0';
+$ilias_max_version = '8.999';
+
+$responsible = 'Jesus Copado';
+$responsible_mail = 'jcopado@surlabs.es';
+
+$supports_export = false;
+$supports_cli_setup = true;
---
+++ sql/dbupdate.php
@@ -0,0 +1,168 @@
+<#1>
+<?php
+//Previous Version
+?>
+<#2>
+<?php
+//Previous Version
+?>
+<#3>
+<?php
+//Previous Version
+?>
+<#4>
+<?php
+//Previous Version
+?>
+<#5>
+<?php
+//Previous Version
+?>
+<#6>
+<?php
+//Previous Version
+?>
+<#7>
+<?php
+//Previous Version
+?>
+<#8>
+<?php
+//Previous Version
+?>
+<#9>
+<?php
+//Previous Version
+?>
+<#10>
+<?php
+//Previous Version
+?>
+<#11>
+<?php
+//Previous Version
+?>
+<#12>
+<?php
+//Previous Version
+?>
+<#13>
+<?php
+//Previous Version
+?>
+<#14>
+<?php
+//Previous Version
+?>
+<#15>
+<?php
+//Previous Version
+?>
+<#16>
+<?php
+//Previous Version
+?>
+<#17>
+<?php
+//Previous Version
+?>
+<#18>
+<?php
+//Previous Version
+?>
+<#19>
+<?php
+//Previous Version
+?>
+<#20>
+<?php
+//Previous Version
+?>
+<#21>
+<?php
+//Previous Version
+?>
+<#22>
+<?php
+//Previous Version
+?>
+<#23>
+<?php
+//Previous Version
+?>
+<#24>
+<?php
+//Previous Version
+?>
+<#25>
+<?php
+//Previous Version
+?>
+<#26>
+<?php
+//Previous Version
+?>
+<#27>
+<?php
+//Previous Version
+?>
+<#28>
+<?php
+//Previous Version
+?>
+<#29>
+<?php
+//Previous Version
+?>
+<#30>
+<?php
+//Previous Version
+?>
+<#31>
+<?php
+//Previous Version
+?>
+<#32>
+<?php
+//Previous Version
+?>
+<#33>
+<?php
+//Previous Version
+?>
+<#34>
+<?php
+//Previous Version
+?>
+<#35>
+<?php
+//Previous Version
+?>
+<#36>
+<?php
+//Previous Version
+?>
+<#37>
+<?php
+//Previous Version
+?>
+<#38>
+<?php
+//Previous Version
+?>
+<#39>
+<?php
+//Previous Version
+?>
+<#40>
+<?php
+//Previous Version
+?>
+<#41>
+<?php
+//Previous Version
+?>
+<#42>
+<?php
+//Current Version
+?>
