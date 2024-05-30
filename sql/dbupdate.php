36acab474a652fcf1cecf858ab6176f6092bcbc5
Saúl Díaz <sauldiazdiaz210@gmail.com>
2024-05-30 14:14:24 +0200

    Update dbupdate.php

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -1,6 +1,227 @@
 <#1>
 <?php
 //Previous Version
+global $DIC;
+$db = $DIC->database();
+if (!$db->tableExists('rep_robj_xlvo_cat')) {
+    $fields = [
+        'id' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => true
+        ],
+        'title' => [
+            'type' => 'text',
+            'length' => 256,
+            'notnull' => false
+        ],
+        'voting_id' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ],
+        'round_id' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ]
+    ];
+
+    $db->createTable('rep_robj_xlvo_cat', $fields);
+    $db->addPrimaryKey('rep_robj_xlvo_cat', ['id']);
+    $db->createSequence('rep_robj_xlvo_cat');
+}
+if (!$db->tableExists('rep_robj_xlvo_config_n')) {
+    $fields = [
+        'obj_id' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => true
+        ],
+        'pin' => [
+            'type' => 'text',
+            'length' => 256,
+            'notnull' => false
+        ],
+        'obj_online' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'anonymous' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'terminable' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'start_date' => [
+            'type' => 'timestamp',
+            'notnull' => false
+        ],
+        'end_date' => [
+            'type' => 'timestamp',
+            'notnull' => false
+        ],
+        'reuse_status' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'last_access' => [
+            'type' => 'timestamp',
+            'notnull' => false
+        ],
+        'frozen_behaviour' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'results_behaviour' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'voting_history' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'show_attendees' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'puk' => [
+            'type' => 'text',
+            'length' => 256,
+            'notnull' => false
+        ]
+    ];
+
+    $db->createTable('rep_robj_xlvo_config_n', $fields);
+    $db->addPrimaryKey('rep_robj_xlvo_config_n', ['obj_id']);
+}
+if (!$db->tableExists('rep_robj_xlvo_option_n')) {
+    $fields = [
+        'text' => [
+            'type' => 'text',
+            'length' => 256,
+            'notnull' => false
+        ],
+        'id' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => true
+        ],
+        'voting_id' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ],
+        'type' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ],
+        'status' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ],
+        'position' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ],
+        'correct_position' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ]
+    ];
+
+    $db->createTable('rep_robj_xlvo_option_n', $fields);
+    $db->addPrimaryKey('rep_robj_xlvo_option_n', ['id']);
+    $db->createSequence('rep_robj_xlvo_option_n');
+}
+if (!$db->tableExists('rep_robj_xlvo_player_n')) {
+    $fields = [
+        'id' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => true
+        ],
+        'obj_id' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ],
+        'active_voting' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ],
+        'status' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ],
+        'frozen' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'timestamp_refresh' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ],
+        'show_results' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'button_states' => [
+            'type' => 'text',
+            'length' => 1024,
+            'notnull' => false
+        ],
+        'countdown' => [
+            'type' => 'integer',
+            'length' => 6,
+            'notnull' => false
+        ],
+        'countdown_start' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ],
+        'force_reload' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => false
+        ],
+        'round_id' => [
+            'type' => 'integer',
+            'length' => 8,
+            'notnull' => false
+        ]
+    ];
+
+    $db->createTable('rep_robj_xlvo_player_n', $fields);
+    $db->addPrimaryKey('rep_robj_xlvo_player_n', ['id']);
+    $db->createSequence('rep_robj_xlvo_player_n');
+}
+// Pendiente: rep_robj_xlvo_round_n
+// Pendiente: rep_robj_xlvo_votehist
+// Pendiente: rep_robj_xlvo_vote_n
+// Pendiente: rep_robj_xlvo_voting_n
+// Pendiente: xlvo_config
+// Pendiente: xlvo_voter
 ?>
 <#2>
 <?php
