9cf4887ea210133a6c265fa3bc16a125d27a935e
Daniel Cazalla (ZallaxDev) <86362063+ZallaxDev@users.noreply.github.com>
2024-06-13 10:35:38 +0200

    Partial implementation of new voting creation menu

--- classes/ui/voting/class.LiveVotingManageUI.php
+++ classes/ui/voting/class.LiveVotingManageUI.php
@@ -23,6 +23,7 @@ namespace LiveVoting\UI;
 use ilCtrlInterface;
 use ILIAS\UI\Factory;
 use ilLiveVotingPlugin;
+use ilObjLiveVotingGUI;
 use ilPlugin;
 use ilSystemStyleException;
 use ilTemplate;
@@ -48,8 +49,49 @@ class LiveVotingManageUI
      * @var ilPlugin
      */
     protected ilPlugin $plugin;
+
+    /**
+     * @throws \ilCtrlException
+     */
     public function showManage(): string{
         global $DIC;
+        $this->plugin = ilLiveVotingPlugin::getInstance();
+        $this->control = $DIC->ctrl();
+
+        $f = $DIC->ui()->factory();
+        $renderer = $DIC->ui()->renderer();
+        $ico = $f->symbol()->icon()->standard('', '')->withSize('medium')->withAbbreviation('+');
+        $image = $f->image()->responsive("src/UI/examples/Image/mountains.jpg", "Image source: https://stocksnap.io, Creative Commons CC0 license");
+        $page = $f->modal()->lightboxImagePage($image, 'Mountains');
+        $modal = $f->modal()->lightbox($page);
+
+        $glyph = $f->symbol()->glyph()->add("#");
+
+        $button = $f->button()->bulky($glyph, '<div style="margin-left:10px">'.$this->plugin->txt("voting_type_1").' <br/><small><muted>('.$this->plugin->txt("voting_type_1_info").')</muted></small></div>', $this->control->getLinkTargetByClass(ilObjLiveVotingGUI::class, 'selectedType1'));
+        $button2 = $f->button()->bulky($glyph, '<div style="margin-left:10px">'.$this->plugin->txt("voting_type_2").' <br/><small><muted>('.$this->plugin->txt("voting_type_2_info").')</muted></small></div>', $this->control->getLinkTargetByClass(ilObjLiveVotingGUI::class, 'selectedType2'));
+        $button3 = $f->button()->bulky($glyph, '<div style="margin-left:10px">'.$this->plugin->txt("voting_type_4").' <br/><small><muted>('.$this->plugin->txt("voting_type_4_info").')</muted></small></div>', $this->control->getLinkTargetByClass(ilObjLiveVotingGUI::class, 'selectedType4'));
+        $button4 = $f->button()->bulky($glyph, '<div style="margin-left:10px">'.$this->plugin->txt("voting_type_5").' <br/><small><muted>('.$this->plugin->txt("voting_type_5_info").')</muted></small></div>', $this->control->getLinkTargetByClass(ilObjLiveVotingGUI::class, 'selectedType5'));
+        $button5 = $f->button()->bulky($glyph, '<div style="margin-left:10px">'.$this->plugin->txt("voting_type_6").' <br/><small><muted>('.$this->plugin->txt("voting_type_6_info").')</muted></small></div>', $this->control->getLinkTargetByClass(ilObjLiveVotingGUI::class, 'selectedType6'));
+
+
+        $uri = new \ILIAS\Data\URI('https://ilias.de');
+        $link = $f->link()->bulky($ico->withAbbreviation('>'), 'Link', $uri);
+        $divider = $f->divider()->horizontal();
+
+        $items = [
+            $f->menu()->sub($this->plugin->txt('voting_add'), [$button, $button2, $button3, $button4, $button5]),
+
+            $f->menu()->sub($this->plugin->txt('voting_reset_all'), [
+                $f->menu()->sub('Otter', [$button, $link]),
+                $f->menu()->sub('Mole', [$button, $link]),
+                $divider,
+                $f->menu()->sub('Deer', [$button, $link])
+            ])
+        ];
+
+        $dd = $f->menu()->drilldown('Manage Votings (NO TRANSLATED)', $items);
+
+        return $renderer->render($dd);
 
         return "TEST";
 
@@ -72,6 +114,7 @@ class LiveVotingManageUI
             ->withRequired(true);
 
         $form_fields[] = $radio;
+
 
         $form = $this->factory->input()->container()->form()->standard('#', $form_fields);
 
