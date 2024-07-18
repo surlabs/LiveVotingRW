3f517afc63536951f23ab54eff84d42b0d3d1c58
Saúl Díaz <sauldiazdiaz210@gmail.com>
2024-07-18 11:22:32 +0200

    Update class.LiveVotingDisplayPlayerUI.php

--- classes/ui/voting/class.LiveVotingDisplayPlayerUI.php
+++ classes/ui/voting/class.LiveVotingDisplayPlayerUI.php
@@ -34,6 +34,7 @@ use LiveVoting\questions\LiveVotingQuestionOption;
 use LiveVoting\UI\QuestionsResults\LiveVotingInputResultsGUI;
 use LiveVoting\Utils\ParamManager;
 use LiveVoting\votings\LiveVoting;
+use LiveVoting\votings\LiveVotingVote;
 use LiveVoting\votings\LiveVotingVoter;
 
 /**
@@ -159,6 +160,8 @@ class LiveVotingDisplayPlayerUI
             $this->tpl->setVariable('COUNTDOWN_CSS', $player->getCountdownClassname());
             $this->tpl->parseCurrentBlock();
         }
+
+        $this->tpl->setVariable('VOTERS_TEXT', vsprintf(ilLiveVotingPlugin::getInstance()->txt("player_voters_description"), [LiveVotingVote::countVoters($player->getActiveVoting(), $player->getRoundId())]));
 
         $this->tpl->setVariable('COUNT', $this->liveVoting->countQuestions());
         $this->tpl->setVariable('POSITION', $this->liveVoting->getQuestionPosition() + 1);
