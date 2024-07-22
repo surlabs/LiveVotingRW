8bccc767da11bce979a28bf1e8d577fcaa917ecd
jcopado <jcopado@surlabs.es>
2024-07-22 22:37:50 +0200

    Format revision ILIAS8

--- error.php
+++ error.php
@@ -1,4 +1,4 @@
 <?php
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $ilias_http = strstr($actual_link, 'Customizing', true);
-header('Location: '.$ilias_http."error.php");
+header('Location: ' . $ilias_http . "error.php");
