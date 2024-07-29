5b7d6605663ad615321d2b25842faa6b8cb18151
Saúl Díaz <sauldiazdiaz210@gmail.com>
2024-07-29 13:03:37 +0200

    Fix old_delos.css implementation

--- classes/platform/ilias/class.LiveVotingInitialisation.php
+++ classes/platform/ilias/class.LiveVotingInitialisation.php
@@ -174,12 +174,15 @@ class LiveVotingInitialisation extends ilInitialisation
         $tpl = new ilGlobalTemplate("tpl.main.html", true, true, "Customizing/global/plugins/Services/Repository/RepositoryObject/LiveVoting", "DEFAULT", true);
 
         $tpl->touchBlock("navbar");
-        $tpl->addCss('./templates/default/delos.css');
-        $tpl->addBlockFile("CONTENT", "content", "tpl.main_voter.html", ilLiveVotingPlugin::getInstance()->getDirectory());
+        $tpl->addCss(ilLiveVotingPlugin::getInstance()->getDirectory() . '/templates/css/old_delos.css');
+        $tpl->addCss(ilLiveVotingPlugin::getInstance()->getDirectory() . '/templates/default/default.css');
+        //$tpl->addCss('/templates/default/030-tools/legacy-bootstrap-mixins/_nav-divider.scss');
+
+        $tpl->addBlockFile("CONTENT", "content", "tpl.main_voter.html", "Customizing/global/plugins/Services/Repository/RepositoryObject/LiveVoting");
+
+        $tpl->setVariable("BASE", LiveVotingConfig::getBaseVoteUrl());
 
         self::initGlobal("tpl", $tpl);
-
-        $tpl->setVariable('BASE', LiveVotingConfig::getBaseVoteURL());
 
         iljQueryUtil::initjQuery();
         ilUIFramework::init();
